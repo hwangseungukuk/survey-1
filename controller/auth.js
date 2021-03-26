aeb2b24b3141932625aad9dbfaa00fe24b571d73
parksangwoo <b01056721617@gmail.com>
2021-03-27 01:59:33 +0900

    [CREATE]

---
+++ controller/auth.js
@@ -0,0 +1,17 @@
+const Survey = require('../models/survey');
+
+const post = async(req, res) => {
+    try {
+        await Survey.create({
+            name: req.body.name,
+            number: req.body.number
+        });
+        res.status(200).json({
+            message: "success"
+        });
+        res.end();
+    } catch(err) {
+        console.error(err);
+        return err;
+    }
+}
