8f645837d2bc5f8a9c6dfbde6f8380f77422d006
parksangwoo <b01056721617@gmail.com>
2021-03-26 21:09:20 +0900

    survey

--- models/index.js
+++ models/index.js
@@ -1,37 +1,19 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const Survey = require('./survey');
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.Survey = Survey;
+
+Survey.init(sequelize, Sequelize);
+
+Survey.associate(db);
 
 module.exports = db;
---
+++ models/survey.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Survey extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            
+        })
+    }
+}
