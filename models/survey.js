3feca24e8f5f098a024bb3ac46c79987cbd03bb9
parksangwoo <b01056721617@gmail.com>
2021-03-27 01:55:26 +0900

    [CREATE] survey

--- models/survey.js
+++ models/survey.js
@@ -3,7 +3,25 @@ const Sequelize = require('sequelize');
 module.exports = class Survey extends Sequelize.Model {
     static init(sequelize) {
         return super.init({
-            
-        })
+            name: {
+                type: Sequelize.STRING(30),
+                allowNull: false
+            },
+            number: {
+                type: Sequelize.STRING(30),
+                allowNull: false
+            },
+            survey: {
+                type: Sequelize.STRING(100),
+                allowNull: false
+            }
+        }, {
+            sequelize,
+            tableName: "survey",
+            modelName: "Survey",
+            paranoid: false,
+            charset: "utf8",
+            collate: "utf8_general_ci"
+        });
     }
 }
