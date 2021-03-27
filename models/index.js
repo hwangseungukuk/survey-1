af14c38de733c5e88d7b098eb2f8aba2acbcde08
parksangwoo <b01056721617@gmail.com>
2021-03-28 03:48:54 +0900

    [PATCH] index.js

--- models/index.js
+++ models/index.js
@@ -1,17 +1,18 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config')[env];
-const db = {};
-
 const Survey = require('./survey');
+
+const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 db.Survey = Survey;
 
-Survey.init(sequelize, Sequelize);
+Survey.init(sequelize);
+
+Survey.associate(db);
 
 module.exports = db;
