47beaecf0d52c735a21e3a6b8b9366d9de8e9257
parksangwoo <b01056721617@gmail.com>
2021-03-27 14:36:09 +0900

    [CREATE] survey.js

--- controller/survey.js
+++ controller/survey.js
@@ -1,1 +1,18 @@
-const 
+const Survey = require('../models/survey');
+
+const getSurvey = async(req, res) => {
+    try {
+        await Survey.create({
+            survey: req.body.survey,
+        });
+        res.status(200);
+        res.end();
+    } catch(error) {
+        console.error(error);
+        return error;
+    }
+};
+
+module.exports = {
+    getSurvey
+}
