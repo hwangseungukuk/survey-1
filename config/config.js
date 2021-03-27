978a4419a043baddf820eb3ade0dcdcf79e322dd
parksangwoo <b01056721617@gmail.com>
2021-03-27 17:51:57 +0900

    [PATCH] config.js

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": "admin",
+    "password": process.env.PASSWORD,
+    "database": "survey",
+    "host": "sourceout.cycfrbihifri.ap-northeast-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    "operatorsAliases": 0,
+    "timezone": "+09:00"
+  },
+  "test": {
+    "username": "admin",
+    "password": process.env.PASSWORD,
+    "database": "survey",
+    "host": "sourceout.cycfrbihifri.ap-northeast-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    "operatorsAliases": 0,
+    "timezone": "+09:00"
+  },
+  "production": {
+    "username": "admin",
+    "password": process.env.PASSWORD,
+    "database": "survey",
+    "host": "sourceout.cycfrbihifri.ap-northeast-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    "operatorsAliases": 0,
+    "timezone": "+09:00"
+  }
+};
