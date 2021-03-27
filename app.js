faab602133f4b8ec725b3dc47a4fc73eacec88e7
parksangwoo <b01056721617@gmail.com>
2021-03-27 21:27:20 +0900

    [PATCH] app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
-const sequelize = require('sequelize');
+const { sequelize } = require('./models');
 
 const app = express();
 
