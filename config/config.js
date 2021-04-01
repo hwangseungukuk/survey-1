74eb0788d5a42a47db269296f9ba6893ca0389b9
parksangwoo <b01056721617@gmail.com>
2021-04-02 08:36:08 +0900

    [PATCH] config.js

--- config/config.js
+++ config/config.js
@@ -2,28 +2,28 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "admin",
+    "username": "root",
     "password": process.env.PASSWORD,
     "database": "outsource",
-    "host": "outsource.cycfrbihifri.ap-northeast-2.rds.amazonaws.com",
+    "host": "sql211.utae.co.kr",
     "dialect": "mysql",
     "operatorsAliases": 0,
     "timezone": "+09:00"
   },
   "test": {
-    "username": "admin",
+    "username": "root",
     "password": process.env.PASSWORD,
     "database": "outsource",
-    "host": "outsource.cycfrbihifri.ap-northeast-2.rds.amazonaws.com",
+    "host": "sql211.utae.co.kr",
     "dialect": "mysql",
     "operatorsAliases": 0,
     "timezone": "+09:00"
   },
   "production": {
-    "username": "admin",
+    "username": "root",
     "password": process.env.PASSWORD,
     "database": "outsource",
-    "host": "outsource.cycfrbihifri.ap-northeast-2.rds.amazonaws.com",
+    "host": "sql211.utae.co.kr",
     "dialect": "mysql",
     "operatorsAliases": 0,
     "timezone": "+09:00"
