ff730511152e8741c6f30c3d61569f7608174857
parksangwoo <b01056721617@gmail.com>
2021-04-07 17:04:09 +0900

    [}

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', express.static(path.join(__dirname, 'html')));
+app.use(express.static(__dirname, '/frontend'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 const sessionOption = {
     resave: false,
@@ -48,6 +48,9 @@ if(process.env.NODE_ENV === 'production') {
 
 app.use(session(sessionOption));
 
+app.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/index.html');
+})
 app.use('/', authRouter);
 app.use('/survey', surveyRouter);
 
