6984b80d4b52ff2a448345415f8c892a6d814686
hwangseungukuk <42322398+hwangseungukuk@users.noreply.github.com>
2021-04-22 11:34:16 +0900

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -3,10 +3,12 @@ const Survey = require('../models/survey');
 const auth = async(req, res) => {
     const name = req.body.name;
     const number = req.body.number;
+    const old = req.body.old;
+    const sex = req.body.sex;
     const time = new Date();
     try {
-        await Survey.create({name: name, number: number, date: time},
-            { fields: ["name", "number", "date"] });
+        await Survey.create({name: name, number: number, date: time, old: old, sex: sex},
+            { fields: ["name", "number", "date", "old", "sex"] });
     } catch(err) {
         console.error(err);
         res.status(400).json({
